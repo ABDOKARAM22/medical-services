c69df53f49ed23b47eab1964153a9c0244b2bb33
Abdelrahman karam <108310836+ABDOKARAM22@users.noreply.github.com>
2022-12-06 18:41:19 +0200

    Update config.php
    chang path

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 
-define('BURL', 'http://localhost:8080/project_3/');
-define('BURLA', 'http://localhost:8080/project_3/admin/');
-define('ASSETS', 'http://localhost:8080/project_3/assets/');
+define('BURL', 'http://localhost:8080/medical-project/');
+define('BURLA', 'http://localhost:8080/medical-project/admin/');
+define('ASSETS', 'http://localhost:8080/medical-project/assets/');
 
 define('DIRA', __DIR__ . '/admin/');
 define('DIR', __DIR__ . '/');
