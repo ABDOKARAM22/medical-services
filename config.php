2ca2d14ecc48015f17742f9240ee7b157371813d
Abdelrahman karam <108310836+ABDOKARAM22@users.noreply.github.com>
2022-12-06 19:12:36 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 
-define('BURL', 'http://localhost:8080/medical-project/');
-define('BURLA', 'http://localhost:8080/medical-project/admin/');
-define('ASSETS', 'http://localhost:8080/medical-project/assets/');
+define('BURL', 'https://abdokaram22.github.io/medical-services/');
+define('BURLA', 'https://abdokaram22.github.io/medical-services/admin/');
+define('ASSETS', 'https://abdokaram22.github.io/medical-services/assets/');
 
 define('DIRA', __DIR__ . '/admin/');
 define('DIR', __DIR__ . '/');
