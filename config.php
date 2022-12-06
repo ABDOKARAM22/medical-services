d8363a9c0e112abaa1051f00d64462068e33ed51
Abdelrahman karam <108310836+ABDOKARAM22@users.noreply.github.com>
2022-12-06 20:23:14 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
 session_start();
 
-define('BURL', 'https://abdokaram22.github.io/medical-services/');
-define('BURLA', 'https://abdokaram22.github.io/medical-services/admin/');
-define('ASSETS', 'https://abdokaram22.github.io/medical-services/assets/');
+define('BURL', 'http://localhost:8080/medical-project/');
+define('BURLA', 'http://localhost:8080/medical-project/admin/');
+define('ASSETS', 'http://localhost:8080/medical-project/assets/');
 
 define('DIRA', __DIR__ . '/admin/');
 define('DIR', __DIR__ . '/');
 
 
 ##connect to database...
-require_once(DIR.'func/db.php');
+require_once(DIR . 'func/db.php');
